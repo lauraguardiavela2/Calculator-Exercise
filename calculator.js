84aa50e9b1214e7266adb6c6b8bcec2fe9061bec
lauraguardiavela2 <laura.guardia@edreamsodigeo.com>
2024-12-18 13:45:06 +0100

    refactor: improve code button onclick

--- calculator.js
+++ calculator.js
@@ -43,54 +43,39 @@ var operation = {
     }
     
 }
-document
-  .querySelector(".calculator")
-  .addEventListener("click", function (event) 
-   {
-        if (!event.target.classList.contains("default-button")) {
-            return;
-        }
-        if (event.target.classList.contains("results-box")) {
-            return;
-        }
-        if(event.target.classList.contains("number")){
-            operation.currentNumber += (event.target.innerText).toString();
-            paragraph.innerText = operation.currentNumber;
-        }
-        if(event.target.classList.contains("operator")){
-            if (operation.currentNumber === "") { return; }
-            var currentOperator = (event.target.innerText).toString();
-            operation.operationList.push(operation.currentNumber);
-            operation.operationList.push(currentOperator);
-            operation.currentNumber = "";
-            paragraph.innerText = currentOperator;
-        }
-        if(event.target.classList.contains("equal")){
-            operation.operationList.push(operation.currentNumber);
-            const result = operation.execute();
-            paragraph.innerText = result.toString();
-            operation.reset();
-        }
-        if(event.target.classList.contains("clean")){
-            operation.reset();
-            paragraph.innerText = "";
-        }
-        if(event.target.classList.contains("delete")){
-            if (operation.currentNumber !== "") {
-                // If we are writting a number, we delete the last number
-                operation.currentNumber = operation.currentNumber.slice(0, -1);
-                paragraph.innerText = operation.currentNumber;
-            } else if (operation.operationList.length > 0) {
-                // If there is not any number, we delete the last element of the list
-                operation.operationList.pop();
-                // We show the last element of the list if there is any
-                if (operation.operationList.length > 0) {
-                    paragraph.innerText = operation.operationList[operation.operationList.length - 1];
-                } else {
-                    paragraph.innerText = "";
-                }
-            }
-        }
+function handleNumberClick(event) {
+    operation.currentNumber += event.target.innerText;
+    paragraph.innerText = operation.currentNumber;
+}
+
+function handleOperatorClick(event) {
+    if (operation.currentNumber === "") return;
+    var currentOperator = event.target.innerText;
+    operation.operationList.push(operation.currentNumber);
+    operation.operationList.push(currentOperator);
+    operation.currentNumber = "";
+    paragraph.innerText = currentOperator;
+}
+
+function handleEqualClick() {
+    operation.operationList.push(operation.currentNumber);
+    const result = operation.execute();
+    paragraph.innerText = result.toString();
+    operation.reset();
+}
+
+function handleCleanClick() {
+    operation.reset();
+    paragraph.innerText = "";
+}
+
+function handleDeleteClick() {
+    if (operation.currentNumber !== "") {
+        operation.currentNumber = operation.currentNumber.slice(0, -1);
+        paragraph.innerText = operation.currentNumber;
+    } else if (operation.operationList.length > 0) {
+        operation.operationList.pop();
+        paragraph.innerText = operation.operationList.length > 0 ? operation.operationList[operation.operationList.length - 1] : "";
     }
-);
+}
 const paragraph = document.querySelector(".results-box");
